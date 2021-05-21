18ea9ac441b20c2ef7a3d80a5843481d8a82984e
Surbhitiwari26 <84510444+Surbhitiwari26@users.noreply.github.com>
2021-05-21 12:32:38 +0530

    Update StringCalculator.java

--- incubyte/src/main/java/co/incubyte/StringCalculator.java
+++ incubyte/src/main/java/co/incubyte/StringCalculator.java
@@ -7,13 +7,25 @@ public class StringCalculator {
 			return 0;
 		} else {
 			String[] number = numbers.split("//|,|\n|;|#|@");
+			String negNum = "";
 			int j = 0;
 			int num;
 			for (int i = 0; i < number.length; i++) {
 				if (!number[i].equals("")) {
 					num = Integer.parseInt(number[i]);
-					j = num + j;
+					if (num >= 0) {
+						j = num + j;
+					} else {
+							if (negNum.equals("")) {
+								negNum = String.valueOf(num);
+							} else {
+								negNum += ("," + String.valueOf(num));
+							}
+					}
 				}
+			}
+			if (!negNum.equals("")) {
+				throw new IllegalArgumentException("Negatives not allowed: " + negNum);
 			}
 			return j;
 		}
