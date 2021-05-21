51dd8505e08ea442ee13ca272eddad92388943fb
Surbhitiwari26 <84510444+Surbhitiwari26@users.noreply.github.com>
2021-05-21 12:28:53 +0530

    Update StringCalculatorTest.java

--- incubyte/src/test/java/co/incubyte/StringCalculatorTest.java
+++ incubyte/src/test/java/co/incubyte/StringCalculatorTest.java
@@ -38,4 +38,9 @@ public class StringCalculatorTest {
 	public void return_Addition_For_String_Separated_By_NewLine_And_Comma(){
 		Assert.assertEquals(20, stringcalculator.Add("6\n3,1\n5,5"));
 	}
+	
+	@Test
+	public void return_Addition_For_String_Contains_Different_Delimiters(){
+		Assert.assertEquals(15, stringcalculator.Add("//3,\n4;1//3#@4"));
+	}
 }
