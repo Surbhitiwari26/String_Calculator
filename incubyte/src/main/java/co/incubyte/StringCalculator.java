1f5ae57a4d1a6846c87c31510152e1642cc29934
Surbhitiwari26 <84510444+Surbhitiwari26@users.noreply.github.com>
2021-05-21 12:29:48 +0530

    Update StringCalculator.java

--- incubyte/src/main/java/co/incubyte/StringCalculator.java
+++ incubyte/src/main/java/co/incubyte/StringCalculator.java
@@ -6,7 +6,7 @@ public class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		} else {
-			String[] number = numbers.split(",|\n");
+			String[] number = numbers.split("//|,|\n|;|#|@");
 			int j = 0;
 			int num;
 			for (int i = 0; i < number.length; i++) {
